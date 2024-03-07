53968816a57442f9365e9ccd95cbd048cbbf9b54
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-03-07 10:31:07 +0530

    add-strings

---
+++ Add-Strings-415.js
@@ -0,0 +1,7 @@
+var addStrings = function(num1, num2) {
+    return String(BigInt(num1)+BigInt(num2))
+};
+
+let num1 = "9333852702227987", num2 = "731737104263"
+
+console.log(addStrings(num1,num2));
---
+++ Single-Number-136.js
@@ -0,0 +1,14 @@
+var singleNumber = function(nums) {
+    let r=[]
+    for (let i = 1; i <= nums.length; i++) {
+        for (let j = i+1; j <= nums.length; j++) {
+            if(nums[i]!==nums[j]){
+                
+            }    
+        }
+    }
+    console.log(r);
+};
+
+let nums = [1,1,2,2,4]
+singleNumber(nums)
