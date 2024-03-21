63c8bc9a3012c434bf41bb2a3f1279dbcb97b296
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-03-21 13:48:26 +0530

    mypow

---
+++ Missing-Number-268.js
@@ -0,0 +1,7 @@
+var missingNumber = function(nums) {
+    strng=nums.toString()
+    console.log();
+
+};
+nums = [9,6,4,2,3,5,7,0,1]
+missingNumber(nums)
---
+++ Pow(x, n)-50.js
@@ -0,0 +1,5 @@
+var myPow = function(x, n) {
+    return Math.pow(x,n)
+};
+let x = 2.00000, n = -2
+myPow(x,n)
