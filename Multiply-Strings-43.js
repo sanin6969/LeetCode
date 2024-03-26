cd7936fe2939fa36f27394dbc95c949b5fcf7a35
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-03-26 14:48:09 +0530

    multiply

---
+++ Multiply-Strings-43.js
@@ -0,0 +1,6 @@
+var multiply = function(num1, num2) {
+
+    return (BigInt(num1)*BigInt(num2)).toString()
+};
+let num1 = "2", num2 = "3"
+multiply(num1,num2)
--- add-two-numbers-2.js
+++ add-two-numbers-2.js
@@ -1,7 +1,15 @@
-var addTwoNumbers = function(l1, l2) {
-        
-
+var addTwoNumbers = function (l1, l2) {
+  r1 = l1.join("")
+  r2 = l2.join("")
+  console.log(r1);
+  sum = parseInt(r1) + parseInt(r2);
+  result = sum.toString().split("");
+  let reversedArray = [];
+  for (let i = result.length - 1; i >= 0; i--) {
+     reversedArray.push(result[i]);
+  }
+  return reversedArray
 };
 let l1 = [2,4,3], l2 = [5,6,4]
 
-addTwoNumbers(l1,l2)
+console.log(addTwoNumbers(l1, l2));
